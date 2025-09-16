cef4a1d31da941c21fb1d648dae97ce4da7208cf
Ítalo <italooo.senai@gmail.com>
2025-09-16 15:01:25 -0300

    add LoginCadastro component for user registration and login functionality; update App title

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   return (
     <>
       <div>
-      
+        
       </div>
-      <h1>Vite + React</h1>
+      <h1>TriStack</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
